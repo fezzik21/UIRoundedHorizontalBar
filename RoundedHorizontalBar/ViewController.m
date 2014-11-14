eba6c04d5a6c4f33544a70aeaeb1c82ac2827965
Adam Hunter <adam@apportable.com>
2014-11-14 13:54:46 -0800

    Fix compile error

--- RoundedHorizontalBar/ViewController.m
+++ RoundedHorizontalBar/ViewController.m
@@ -22,11 +22,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
     self.bar = [UIRoundedHorizontalBar barWithFrame:CGRectMake(20, 100, 280, 40)
                                            andCount:3
                                           andTitles:@[@"V1", @"V2", @"V3"]
                                             andFont:[UIFont fontWithName:@"Avenir-Book" size:15.0f]
-                                       andTextColor:[UIColor whiteColor]
+                               andSelectedTextColor:[UIColor darkGrayColor]
+                             andDeselectedTextColor:[UIColor whiteColor]
                                    andSelectedColor:[UIColor colorWithRed:160.0/255.0 green:170.0/255.0 blue:170.0/255.0 alpha:1.0]
                                  andDeselectedColor:[UIColor colorWithRed:110.0/255.0 green:110.0/255.0 blue:110.0/255.0 alpha:1.0]
                                   andSeperatorColor:[UIColor colorWithRed:40.0/255.0 green:40.0/255.0 blue:50.0/255.0 alpha:1.0]
